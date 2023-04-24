f7acbc01d1c006b860fe4433db7463d176c3ecd1
ykm989 <ykm989@naver.com>
2023-04-24 18:09:30 +0900

    📝 var -> let

--- MyCreditManager/MyCreditManager/main.swift
+++ MyCreditManager/MyCreditManager/main.swift
@@ -14,12 +14,12 @@ var studentInfo = [String:[String:String]]()
 
 while continueFlag{
     print("원하는 기능을 입력해주세요\n1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X: 종료")
-    var inputValue = readLine()
+    let inputValue = readLine()
     if ["1","2","3","4","5","X"].contains(inputValue){
         switch inputValue{
         case "1":
             print("추가할 학생의 이름을 입력해주세요")
-            var studentName: String = readLine()!
+            let studentName: String = readLine()!
             
             if studentName.isEmpty{
                 print("입력이 잘못되었습니다. 다시 입력해주세요.")
@@ -31,7 +31,7 @@ while continueFlag{
             }
         case "2":
             print("삭제할 학생의 이름을 입력해주세요")
-            var studentName: String = readLine()!
+            let studentName: String = readLine()!
             
             if studentName.isEmpty{
                 print("입력이 잘못되었습니다. 다시 입력해주세요.")
@@ -45,7 +45,7 @@ while continueFlag{
             }
         case "3":
             print("성적을 추가할 학생의 이름, 과목 이름, 성적(A+, A, F 등)을 띄어쓰기로 구분하여 차례로 작성해주세요.")
-            var inputInfo = readLine()!.split(separator: " ")
+            let inputInfo = readLine()!.split(separator: " ")
             print("만약에 학생의 성적 중 해당 과목이 존재하면 기존 점수가 갱신됩니다.")
             
             if inputInfo.isEmpty || inputInfo.count != 3 || !studentInfo.keys.contains(String(inputInfo[0])) || !["A+", "A", "B+", "B", "C+", "C", "D+", "D", "F"].contains(String(inputInfo[2])){
@@ -55,7 +55,7 @@ while continueFlag{
                 print("\(inputInfo[0]) 학생의 \(inputInfo[1]) 과목이 \(inputInfo[2])로 추가(변경)되었습니다.")
             }
         case "4":
-            var inputInfo = readLine()!.split(separator: " ")
+            let inputInfo = readLine()!.split(separator: " ")
             
             if inputInfo.isEmpty || inputInfo.count != 2{
                 print("입력이 잘못되었습니다. 다시 입력해주세요.")
@@ -68,14 +68,14 @@ while continueFlag{
             }
         case "5":
             print("평점을 알고싶은 학생의 이름을 입력해주세요.")
-            var studentName: String = readLine()!
+            let studentName: String = readLine()!
             
             if studentName.isEmpty{
                 print("입력이 잘못되었습니다. 다시 확인해주세요.")
             } else if !studentInfo.keys.contains(studentName){
                 print("\(studentName) 학생을 찾지 못했습니다.")
             } else {
-                var subjectList = studentInfo[studentName]!
+                let subjectList = studentInfo[studentName]!
                 var sum = 0.0
                 
                 for i in subjectList{
